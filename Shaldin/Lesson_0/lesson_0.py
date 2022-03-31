e21cb6ade8c0f2c8c04d584a93a98f537d5bb145
zloychan <zloychan@gmail.com>
2022-03-31 14:42:11 +0300

    Lesson 0 + tests

---
+++ Shaldin/Lesson_0/lesson_0.py
@@ -0,0 +1,12 @@
+def return_value():
+    """Функция возвращает переменную"""
+    hello = 'Hello'
+    return hello
+
+
+def main():
+    print(return_value())
+
+
+if __name__ == '__main__':
+    main()
